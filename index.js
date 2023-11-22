c066fb367f1ac8d72da5055e300a31bdb928d5b2
juanca2805 <juan.pedraza2805@gmail.com>
2023-11-22 11:33:37 -0500

    creacion servidor para graphqld prueba

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // index.js
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 const app = express();
 const PORT = 3000;
 
@@ -8,6 +11,23 @@ app.use((req, res, next) => {
   console.log('Middleware ejecutado');
   next();
 });
+
+// Ruta de ejemplo con GraphQL
+const schema = buildSchema(`
+  type Query {
+    message: String
+  }
+`);
+
+const root = {
+  message: () => '¡Hola, mundo desde GraphQL!',
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true, // Habilita la interfaz de GraphiQL para probar consultas
+}));
 
 // Ruta de ejemplo
 app.get('/', (req, res) => {
