5fb34965344a4cf59502f104523e87fe83d5b3b0
juanca2805 <juan.pedraza2805@gmail.com>
2023-11-22 13:03:55 -0500

    Se agregan el componente principal y secundario

---
+++ Client/mi-aplicacion-angular/src/app/secundario/secundario.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-secundario',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './secundario.component.html',
+  styleUrl: './secundario.component.css'
+})
+export class SecundarioComponent {
+
+}
