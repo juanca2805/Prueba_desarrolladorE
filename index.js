18b7ee017f36decbddd0a91a43e0be098ad271d1
juanca2805 <juan.pedraza2805@gmail.com>
2023-11-22 12:07:27 -0500

    Se hace conexion a la api y se ponen los datos en forma de esquema grafhql

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 const axios = require('axios');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const PORT = 3000;
@@ -11,20 +13,57 @@ app.use((req, res, next) => {
   next();
 });
 
-// Ruta para consumir la API "An API of Ice And Fire" y mostrar datos de un personaje
-app.get('/got-character', async (req, res) => {
-  try {
-    // Hacer una solicitud a la API para obtener información sobre Tyrion Lannister
-    const response = await axios.get('https://anapioficeandfire.com/api/characters/2');
-
-    // Mostrar los datos en la respuesta
-    res.json(response.data);
-  } catch (error) {
-    // Manejar errores
-    console.error('Error al hacer la solicitud a la API:', error.message);
-    res.status(500).json({ error: 'Error al obtener datos de la API' });
+// Definir el esquema GraphQL
+const schema = buildSchema(`
+  type Character {
+    id: Int
+    firstName: String
+    lastName: String
+    fullName: String
+    title: String
+    family: String
+    image: String
+    imageUrl: String
   }
-});
+
+  type Query {
+    getCharacter(id: Int!): Character
+  }
+`);
+
+// Resolver para la consulta
+const root = {
+  getCharacter: async ({ id }) => {
+    try {
+      // Hacer una solicitud a la API para obtener información sobre el personaje con el ID proporcionado
+      const response = await axios.get(`https://thronesapi.com/api/v2/Characters/${id}`);
+
+      // Estructurar los datos de respuesta en el formato del esquema GraphQL
+      const characterData = response.data;
+      return {
+        id: characterData.id,
+        firstName: characterData.firstName,
+        lastName: characterData.lastName,
+        fullName: characterData.fullName,
+        title: characterData.title,
+        family: characterData.family,
+        image: characterData.image,
+      
+      };
+    } catch (error) {
+      // Manejar errores
+      console.error('Error al hacer la solicitud a la API:', error.message);
+      throw new Error('Error al obtener datos de la API');
+    }
+  },
+};
+
+// Ruta para GraphQL
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true, // Habilita la interfaz de GraphiQL para probar consultas
+}));
 
 // Ruta de ejemplo
 app.get('/', (req, res) => {
