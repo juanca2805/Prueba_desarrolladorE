89e9f70361276759d365e02ef4ae3468d92a9bf5
juanca2805 <juan.pedraza2805@gmail.com>
2023-11-22 12:53:08 -0500

    Se agrega ruta para mostrar todos los personajes por url

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// index.js
+
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+//axios para manejar las consultas http
 const axios = require('axios');
 const { buildSchema } = require('graphql');
 
 const app = express();
 const PORT = 3000;
 
-// Middleware de ejemplo
+// Middleware 
 app.use((req, res, next) => {
   console.log('Middleware ejecutado');
   next();
 });
 
-// Definir el esquema GraphQL
+// Define el esquema GraphQL
 const schema = buildSchema(`
   type Character {
     id: Int
@@ -28,6 +29,7 @@ const schema = buildSchema(`
 
   type Query {
     getCharacter(id: Int!): Character
+    getAllCharacters: [Character]
   }
 `);
 
@@ -47,32 +49,73 @@ const root = {
         fullName: characterData.fullName,
         title: characterData.title,
         family: characterData.family,
-       
-      
+        image: characterData.image,
+        imageUrl: characterData.imageUrl,
       };
     } catch (error) {
-      // Manejar errores
+      
+      console.error('Error al hacer la solicitud a la API:', error.message);
+      throw new Error('Error al obtener datos de la API');
+    }
+  },
+  getAllCharacters: async () => {
+    try {
+      // Hacer una solicitud a la API para obtener todos los personajes
+      const response = await axios.get('https://thronesapi.com/api/v2/Characters');
+
+      // Estructurar los datos de respuesta en el formato del esquema GraphQL
+      const charactersData = response.data;
+      return charactersData.map(character => ({
+        id: character.id,
+        firstName: character.firstName,
+        lastName: character.lastName,
+        fullName: character.fullName,
+        title: character.title,
+        family: character.family,
+        image: character.image,
+        imageUrl: character.imageUrl,
+      }));
+    } catch (error) {
+    
       console.error('Error al hacer la solicitud a la API:', error.message);
       throw new Error('Error al obtener datos de la API');
     }
   },
 };
 
-// Ruta para GraphQL
+// Aqui se muestran los datos de la api por graphql si se necesita algun query hay un txt de ejemplo
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
-  graphiql: true, // Habilita la interfaz de GraphiQL para probar consultas
+  graphiql: true, // Habilita la interfaz de GraphiQL 
 }));
 
-// Ruta de ejemplo
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+// Ruta de ejemplo para mostrar todos los personajes
+app.get('/characters', async (req, res) => {
+  try {
+    // Hacer una solicitud a la API para obtener todos los personajes
+    const response = await axios.get('https://thronesapi.com/api/v2/Characters');
+
+    // Estructurar los datos 
+    const charactersData = response.data;
+    const characterList = charactersData.map(character => ({
+      id: character.id,
+      fullName: character.fullName,
+      title: character.title,
+      imageUrl: character.imageUrl,
+    }));
+    res.json(characterList);
+  } catch (error) {
+    // Manejar errores
+    console.error('Error al hacer la solicitud a la API:', error.message);
+    res.status(500).json({ error: 'Error al obtener datos de la API' });
+  }
 });
 
-// Iniciar el servidor
+// Iniciar el servidor e imprimir la URL aqui se muestran todos los datos de la api 
 const server = app.listen(PORT, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log(`Servidor escuchando en http://:${port}`);
-  });
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Servidor escuchando en http://localhost:${port}/characters`);
+  
+});
